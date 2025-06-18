3bcb45360816541e1a3e16869c71fdecf6f09b3f
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-06-17 21:54:19 -0300

    Update Layout component background color to white

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ type LayoutProps = {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 font-sans">
+    <div className="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100 font-sans">
       {children}
     </div>
   );
